4b8c04a06d2096b8a0a084a319d2d9ef546b6b4c
mamontovgod-commits <mamontovgod@gmail.com>
2026-03-24 19:35:08 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def catalog_handler(call: types.CallbackQuery):
 async def liquids_handler(call: types.CallbackQuery):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=f"{get_status_emoji(False)} Joker 7% - 600 руб", url=f"https://t.me/{OWNER[1:]}")],
-         [InlineKeyboardButton(text=f"{get_status_emoji(True)} Подонки critical 7% - 600 руб", url=f"https://t.me/{OWNER[1:]}")],
+         [InlineKeyboardButton(text=f"{get_status_emoji(False)} Подонки critical 7% - 600 руб", url=f"https://t.me/{OWNER[1:]}")],
         [InlineKeyboardButton(text=f"{get_status_emoji(False)} Грех/истерика 5% - 500 руб", url=f"https://t.me/{OWNER[1:]}")],
         [InlineKeyboardButton(text=f"{get_status_emoji(False)} Annima love 6% - 550 руб", url=f"https://t.me/{OWNER[1:]}")],
         [InlineKeyboardButton(text=f"{get_status_emoji(False)} Дуал 5% - 500 руб", url=f"https://t.me/{OWNER[1:]}")],
